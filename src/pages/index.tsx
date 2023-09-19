ca1e262623f38d2ad0d97d8339a271abc8ee98fa
hcheng826 <lilioo826@gmail.com>
2023-09-19 23:34:19 +0800

    operation list at home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,40 @@
 import { Text } from '@chakra-ui/react'
+import { CardList } from 'components/layout/CardList'
 import { Head } from 'components/layout/Head'
 import { HeadingComponent } from 'components/layout/HeadingComponent'
 import React from 'react'
+import EtherIcon from 'assets/icons/ethereum.png'
+import NFTIcon from 'assets/icons/nft.png'
+import CartIcon from 'assets/icons/cart.png'
+import WithdrawIcon from 'assets/icons/withdraw.png'
+
+export const OperationItems = [
+  {
+    title: 'List NFT',
+    description: 'NFT sellers can import their own NFT or mint test NFT to list it on the exchange',
+    image: NFTIcon.src,
+    url: '/list-nft',
+  },
+  {
+    title: 'Deposit ETH',
+    description: 'NFT buyers deposit ETH to be eligible for purchasing NFTs',
+    image: EtherIcon.src,
+    url: '/deposit-eth',
+  },
+  {
+    title: 'Buy NFT',
+    description: 'With the secret associated with a valid ETH deposit, buyer can buy NFT and receive it at a desired address',
+    image: CartIcon.src,
+    url: '/buy-nft',
+  },
+  {
+    title: 'NFT Seller Claim ETH',
+    description:
+      'With the secret associated with a valid NFT listing and after the NFT is sold. Seller can claim the ETH and receive it at a desired address',
+    image: WithdrawIcon.src,
+    url: '/withdraw-eth',
+  },
+]
 
 export default function Home() {
   return (
@@ -10,7 +43,9 @@ export default function Home() {
 
       <main>
         <HeadingComponent as="h2">Anon Exchange</HeadingComponent>
-        <Text>A showcase of how Semaphore protocol can be applied</Text>
+        <Text>An anonomyous NFT exchange based on Semaphore protocol</Text>
+        <br />
+        <CardList title="Operations" items={OperationItems} />
       </main>
     </>
   )
